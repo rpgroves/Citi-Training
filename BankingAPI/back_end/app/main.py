47a5648d3724e4a8130cc0248311ac5ba2118209
Rylee Groves <43833545+rpgroves@users.noreply.github.com>
2026-06-03 17:34:30 -0400

    React (Vite) project initialized

--- BankingAPI/back_end/app/main.py
+++ BankingAPI/back_end/app/main.py
@@ -3,12 +3,22 @@ from fastapi import HTTPException
 from contextlib import asynccontextmanager
 from api.routes import customers_controller, accounts_controller, routes
 from db.database import customers_collection
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(customers_controller.router)
 app.include_router(accounts_controller.router)
 app.include_router(routes.router)
